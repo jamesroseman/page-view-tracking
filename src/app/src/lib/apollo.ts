5594558dffc25bb6be3fb4136be986d981649f92
James Roseman <james.roseman@gmail.com>
2021-10-08 17:54:41 +0100

    Added new frontend React server which connects successfully to the data layer

---
+++ src/app/src/lib/apollo.ts
@@ -0,0 +1,18 @@
+import ApolloClient from 'apollo-boost';
+
+import EnvVarUtils from '../utils/EnvVarUtils';
+
+// Get the API URL and port
+const apiUrl: string = EnvVarUtils.getApiUrl();
+const apiPort: number = EnvVarUtils.getApiPort();
+const uri: string = !apiUrl.startsWith('http') ? `http://${apiUrl}:${apiPort}` : `${apiUrl}`;
+
+console.log(uri);
+
+// Disable caching of responses
+// https://github.com/apollographql/apollo-client/issues/3234
+// apollo-boost clients don't include defaultOptions
+// https://github.com/apollographql/apollo-client/issues/3900
+const client = new ApolloClient({ uri });
+
+export default client;
